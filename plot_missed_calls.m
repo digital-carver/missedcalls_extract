e1d3b7289d924ed0ea306f5369c8d0b8cff8e720
SundaraRaman R <sundar@abiteasier.in>
2015-08-26 23:27:53 +0530

    unknown and known calls marked with colors

--- plot_missed_calls.m
+++ plot_missed_calls.m
@@ -11,11 +11,18 @@ x = datenum(datestrings, 'dd-mmm-yyyy');
 y = hr*3600 + min*60 + sec;
 
 unknown_calls = strcmp(names, '(Unknown) ');
+known_calls   = ~unknown_calls;
 
+pl = plot(x, y, '-k', 'LineWidth', 0.01);
+hold on;
+pu = plot(x(unknown_calls), y(unknown_calls), 'o', 'MarkerEdgeColor','k',...
+                                                    'MarkerFaceColor',[1 .2 .3],...
+                                                    'MarkerSize',7);
 
-p = plot(x, y, '-xk');
+pk = plot(x(known_calls), y(known_calls), 'o', 'MarkerEdgeColor','k',...
+                                                    'MarkerFaceColor',[.2 1 .3],...
+                                                    'MarkerSize',7);
 
-set(p, 'LineWidth', 0.01);
 set(gca, 'LineWidth', 2);
 
 monthnums    = month(x);
